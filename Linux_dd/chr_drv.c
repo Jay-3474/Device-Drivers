70a127c15da9117ad26755787f3de2a76611f89c
Jay Chitroda <jaychitroda95@gmail.com>
2026-01-20 13:05:36 +0530

    Refactor chr_drv.c for static major/minor allocation
    Updated character device driver to use static allocation for major and minor numbers.

--- Linux_dd/chr_drv.c
+++ Linux_dd/chr_drv.c
@@ -1,14 +1,17 @@
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/init.h>
-#include <linux/fs.h>
-#include <linux/kdev_t.h>
-#include <linux/cdev.h>
-#include <linux/device.h>
-#include <linux/slab.h>
-#include <linux/uaccess.h>
+#include <linux/kernel.h> // printk
+#include <linux/module.h> // module_license, module_author, module_description
+#include <linux/init.h> // module_init, module_exit
+#include <linux/fs.h> // open, read, write, ioctl, release, struct file_operations, register_chrdev_region, unregister_chrdev_region, register_chrdev, alloc_chrdev_region
+#include <linux/kdev_t.h> // dev_t, major, minor, mkdev
+#include <linux/cdev.h> // cdev_init, cdev_del, cdev_add, cdev_alloc, struct cdev
+#include <linux/device.h> // device_create, class_create, struct device, struct class, device_destroy
+#include <linux/slab.h> // kmalloc, kfree
+#include <linux/uaccess.h> // copy_to_user, copy_from_user
 
 #define mem_size 1024
+#define MY_MAJOR 303 // use while static allocation, check this number if it's not allocated to another device: use cat /proc/devices 
+#define MY_MINOR 0 // use while static allocation
+#define DEVICE_COUNT 1 // use while static allocation
 
 static int __init chr_driver_init(void);
 static void __exit chr_driver_exit(void);
@@ -17,7 +20,8 @@ static int my_release(struct inode *inode, struct file *file);
 static ssize_t my_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
 static ssize_t my_write(struct file *filp, const char __user *buf, size_t len, loff_t *off);
 
-dev_t dev = 0;
+static dev_t my_device_id; // use while static allocation
+//dev_t dev = 0; // use while dynamic allocation
 static struct class *dev_class;
 static struct cdev my_cdev;
 uint8_t *kernel_buffer;
@@ -67,19 +71,38 @@ static ssize_t my_write(struct file *filp, const char __user *buf, size_t len, l
 
 static int __init chr_driver_init(void)
 {
-	/* Allocating Major Number */
-	if((alloc_chrdev_region(&dev, 0, 1, "my_dev")) < 0)
+	int ret;
+
+	////////////////////////////////////////////////////////////////////////////////////////////
+	/* Allocating Major Number Dynamically*/
+	//ret = alloc_chrdev_region(&dev, 0, 1, "my_dev");
+	//if(ret < 0)
+	//{
+	//	printk(KERN_INFO "Cannot alocate the major number....\n ");
+	//	return -1;
+	//}
+	/////////////////////////////////////////////////////////////////////////////////////////////
+
+	/* Allocating Major Number Statically */
+	my_device_id= MKDEV(MY_MAJOR, MY_MINOR);
+	ret = register_chrdev_region(my_device_id, DEVICE_COUNT, "my_dev");
+	if(ret < 0)
 	{
-		printk(KERN_INFO "Cannot alocate the major number....\n ");
+		printk(KERN_INFO "Cannot register the major number....\n");
 		return -1;
 	}
-	printk(KERN_INFO "Major = %d Minor=%d...\n", MAJOR(dev), MINOR(dev));
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	// printk(KERN_INFO "Major = %d Minor=%d...\n", MAJOR(dev), MINOR(dev)); //enable while using dynamic allocation
+	printk(KERN_INFO "Major = %d Minor = %d ...\n", MY_MAJOR, MY_MINOR); //enable while using static allocation
 
 	/* Create cdev structure */
 	cdev_init(&my_cdev, &fops);
 
 	/*Add character device to the system*/
-	if((cdev_add(&my_cdev, dev, 1)) < 0)
+	//if((cdev_add(&my_cdev, dev, 1)) < 0) // enable while using dynamic allocation
+	if((cdev_add(&my_cdev, my_device_id, DEVICE_COUNT)) < 0) //enable while using static allocation
 	{
 		printk(KERN_INFO "Cannot add the device to the system...\n");
 		goto r_class;
@@ -94,7 +117,8 @@ static int __init chr_driver_init(void)
 	}
 
 	/*Create device*/
-	if((device_create(dev_class, NULL, dev, NULL, "my_device")) == NULL)
+	if((device_create(dev_class, NULL, my_device_id, NULL, "my_device")) == NULL) // enable while using static allocation
+	// if((device_create(dev_class, NULL, dev, NULL, "my_device")) == NULL) // enable while using dyamic allocation
 	{
 		printk(KERN_INFO "Cannot create the device\n");
 		goto r_device;
@@ -105,16 +129,19 @@ static int __init chr_driver_init(void)
 r_device:
 	class_destroy(dev_class);
 r_class:
-	unregister_chrdev_region(dev,1);
+	unregister_chrdev_region(my_device_id, DEVICE_COUNT); // enable while using static allocation
+	// unregister_chrdev_region(dev,1); // enable while using dynamic allocation
 	return -1;
 }
 
 static void __exit chr_driver_exit(void)
 {
-	device_destroy(dev_class,dev);
+	device_destroy(dev_class, my_device_id); // enable while using static allocation
+	//device_destroy(dev_class,dev); // enable while using dynamic allocation
 	class_destroy(dev_class);
 	cdev_del(&my_cdev);
-	unregister_chrdev_region(dev,1);
+	unregister_chrdev_region(my_device_id, 1); // enable while using static allocation
+	//unregister_chrdev_region(dev,1); // enable while using dynamic allocation
 	printk(KERN_INFO "Device driver is removed successfully....\n");
 }
 
