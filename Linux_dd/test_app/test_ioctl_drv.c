d0be6aa243f80181bc95b55a58d4749b3e4d15ac
Jay Chitroda <jaychitroda95@gmail.com>
2026-01-21 12:51:42 +0530

    Add test_ioctl_drv.c for IOCTL driver interaction
    Implement a user-space application for IOCTL operations with a character device driver.

---
+++ Linux_dd/test_app/test_ioctl_drv.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+#define WR_DATA _IOW('a', 'a', int32_t*)
+#define RD_DATA _IOR('a', 'b', int32_t*)
+
+int main()
+{
+	int fd;
+	int32_t val, num;
+	printf("\n IOCTL based character device driver operaion from user space...\n");
+	fd = open("/dev/my_device", O_RDWR);
+
+	if(fd < 0)
+	{
+		printf("cannot open the device...\n");
+		return 0;
+	}
+	printf("Enter the data to send...\n");
+	scanf("%d", &num);
+	printf("writing value to the driver...\n");
+	ioctl(fd, WR_DATA, (int32_t*)&num);
+
+	printf("Reading value from the driver...\n");
+	ioctl(fd, RD_DATA, (int32_t*)&val);
+	printf("value = %d\n", val);
+
+	printf("closing the driver...\n");
+	close(fd);
+	return 0;
+}
