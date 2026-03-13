b3513ca45427cb826c11b86a9d4218bfd96ca109
Jay Chitroda <jaychitroda95@gmail.com>
2026-03-13 13:13:15 +0530

    Add interrupt bottom driver module
    This is a Linux kernel module that implements a character device driver with interrupt handling and tasklet support.

---
+++ Linux_dd/interrupt_bottom_drv.c
@@ -0,0 +1,152 @@
+#include <linux/kernel.h> // printk
+#include <linux/module.h> // module_license, module_author, module_description
+#include <linux/init.h> // module_init, module_exit
+#include <linux/fs.h> // open, read, write, ioctl, release, struct file_operations, register_chrdev_region, unregister_chrdev_region, register_chrdev, alloc_chrdev_region
+#include <linux/kdev_t.h> // dev_t, major, minor, mkdev
+#include <linux/cdev.h> // cdev_init, cdev_del, cdev_add, cdev_alloc, struct cdev
+#include <linux/device.h> // device_create, class_create, struct device, struct class, device_destroy
+#include <linux/slab.h> // kmalloc, kfree
+#include <linux/uaccess.h> // copy_to_user, copy_from_user
+#include <linux/interrupt.h>
+#include <asm/io.h>
+
+#define IRQ_NO 1
+unsigned int i = 0;
+#define mem_size 1024
+#define MY_MAJOR 303 // use while static allocation, check this number if it's not allocated to another device: use cat /proc/devices 
+#define MY_MINOR 0 // use while static allocation
+#define DEVICE_COUNT 1 // use while static allocation
+
+static int __init chr_driver_init(void);
+static void __exit chr_driver_exit(void);
+void tasklet_func(unsigned long data);
+
+struct tasklet_struct *tasklet;
+
+/*Declare the tasklet*/
+//DECLARE_TASKLET(tasklet, tasklet_func);
+
+/*Tasklet function body*/
+void tasklet_func(unsigned long data)
+{
+	printk(KERN_INFO "Executing the tasklet function data = %ld\n", data);
+}
+
+static dev_t my_device_id; // use while static allocation
+//dev_t dev = 0; // use while dynamic allocation
+static struct class *dev_class;
+static struct cdev my_cdev;
+uint8_t *kernel_buffer;
+
+static struct file_operations fops =
+{
+	.owner = THIS_MODULE
+};
+
+static irqreturn_t irq_handler(int irq, void* dev_id)
+{
+	printk(KERN_INFO "Keyboard: interrupt occurred %d\n", i);
+	i++;
+	tasklet_schedule(tasklet);
+	return IRQ_HANDLED;
+}
+
+static int __init chr_driver_init(void)
+{
+	int ret;
+
+	////////////////////////////////////////////////////////////////////////////////////////////
+	/* Allocating Major Number Dynamically*/
+	//ret = alloc_chrdev_region(&dev, 0, 1, "my_dev");
+	//if(ret < 0)
+	//{
+	//	printk(KERN_INFO "Cannot alocate the major number....\n ");
+	//	return -1;
+	//}
+	/////////////////////////////////////////////////////////////////////////////////////////////
+
+	/* Allocating Major Number Statically */
+	my_device_id= MKDEV(MY_MAJOR, MY_MINOR);
+	ret = register_chrdev_region(my_device_id, DEVICE_COUNT, "my_dev");
+	if(ret < 0)
+	{
+		printk(KERN_INFO "Cannot register the major number....\n");
+		return -1;
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+	// printk(KERN_INFO "Major = %d Minor=%d...\n", MAJOR(dev), MINOR(dev)); //enable while using dynamic allocation
+	printk(KERN_INFO "Major = %d Minor = %d ...\n", MY_MAJOR, MY_MINOR); //enable while using static allocation
+
+	/* Create cdev structure */
+	cdev_init(&my_cdev, &fops);
+
+	/*Add character device to the system*/
+	//if((cdev_add(&my_cdev, dev, 1)) < 0) // enable while using dynamic allocation
+	if((cdev_add(&my_cdev, my_device_id, DEVICE_COUNT)) < 0) //enable while using static allocation
+	{
+		printk(KERN_INFO "Cannot add the device to the system...\n");
+		goto r_class;
+	}
+
+	/*Create struct class*/
+	dev_class = class_create("test_class");
+	if(dev_class == 0)
+	{
+		printk(KERN_INFO "Cannot create the struct class...\n");
+		goto r_class;
+	}
+
+	/*Create device*/
+	if((device_create(dev_class, NULL, my_device_id, NULL, "my_device")) == NULL) // enable while using static allocation
+	// if((device_create(dev_class, NULL, dev, NULL, "my_device")) == NULL) // enable while using dyamic allocation
+	{
+		printk(KERN_INFO "Cannot create the device\n");
+		goto r_device;
+	}
+	
+	if(request_irq(IRQ_NO, irq_handler, IRQF_SHARED, "my_device", (void *)(irq_handler)))
+	{
+		printk(KERN_INFO "my_device cannot register IRQ\n");
+		goto irq;
+	}
+	tasklet = kmalloc(sizeof(struct tasklet_struct), GFP_KERNEL);
+	if(tasklet == NULL)
+	{
+		printk(KERN_INFO "cannot allocate the tasklet memory\n");
+		goto irq;
+	}
+	
+	tasklet_init(tasklet, tasklet_func, 0); // Dynamic method
+	printk(KERN_INFO "Device driver insert.... done properly...\n");
+	return 0;
+irq:
+	free_irq(IRQ_NO, (void *)(irq_handler));
+r_device:
+	class_destroy(dev_class);
+r_class:
+	unregister_chrdev_region(my_device_id, DEVICE_COUNT); // enable while using static allocation
+	// unregister_chrdev_region(dev,1); // enable while using dynamic allocation
+	return -1;
+}
+
+static void __exit chr_driver_exit(void)
+{
+	free_irq(IRQ_NO, (void *)(irq_handler));
+	tasklet_kill(tasklet);
+	device_destroy(dev_class, my_device_id); // enable while using static allocation
+	//device_destroy(dev_class,dev); // enable while using dynamic allocation
+	class_destroy(dev_class);
+	cdev_del(&my_cdev);
+	unregister_chrdev_region(my_device_id, 1); // enable while using static allocation
+	//unregister_chrdev_region(dev,1); // enable while using dynamic allocation
+	printk(KERN_INFO "Device driver is removed successfully....\n");
+}
+
+module_init(chr_driver_init);
+module_exit(chr_driver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Jay");
+MODULE_DESCRIPTION("My first interrupt Driver");
