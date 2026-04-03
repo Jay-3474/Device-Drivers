c4783c754ea476063c15476acf045d0653295988
Jay Chitroda <jaychitroda95@gmail.com>
2026-04-03 19:47:30 +0530

    Implement device read functionality in dev_read.c
    This program reads data from a specified device file, allowing the user to specify the number of bytes to read. It handles errors during file operations and prints the total bytes read.

---
+++ Beagle_dd/test_app/dev_read.c
@@ -0,0 +1,100 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char buffer[2048];
+
+#define TRY_READ 10
+
+int main(int argc, char *argv[])
+{
+	int fd;
+
+	/*this variable holds remaining data bytes to the read*/
+	int remaining = TRY_READ;
+
+	/*holds count of total data bytes read so far*/
+	int total_read = 0;
+
+	int n=0,ret=0;
+
+	if(argc != 2)
+	{
+		printf("wrong usage\n");
+		printf("correct usage: <file> <readcount>\n");
+		goto out;
+	}
+
+	remaining= atoi(argv[1]);
+	printf("read requested = %d\n", remaining);
+
+	fd = open("/dev/pcdev-3", O_RDONLY);
+	if(fd < 0)
+	{
+		/*perror decodes user space errno variable and prints cause of failure*/
+		perror("open");
+		goto out;
+	}
+	
+	printf("open was successful\n");
+
+#if 1
+	/*activate this for lseek testing*/
+	ret = lseek(fd, 10, SEEK_SET);
+	if(ret < 0)
+	{
+		perror("lseek");
+		goto close_fd;
+	}
+#endif
+	
+	/*lets atrempt reading twise*/
+	while(n != 2 && remaining)
+	{
+		/*read data from fd*/
+		ret  = read(fd, &buffer[total_read], remaining);
+
+		if(!ret)
+		{
+			/*there is nothing to read*/
+			printf("end of file\n");
+			break;
+		}
+		else if(ret <= remaining)
+		{
+			printf("read %d bytes of data\n", ret);
+			/*ret contains count of data bytes successfully read, so and it to total_read*/
+			total_read += ret;
+			/*we read some data, so decrement remaining*/
+			remaining -= ret;
+		}
+		else if(ret < 0)
+		{
+			printf("something went wrong\n");
+			break;
+		}
+		else
+		{
+			break;
+		}
+		n++;
+	}
+	printf("total_read = %d\n", total_read);
+
+	//dump buffer
+	for(int i = 0; i < total_read; i++)
+	{
+		printf("%c", buffer[i]);
+	
+	}
+
+close_fd:
+	close(fd);
+out:
+	return 0;
+}
+
